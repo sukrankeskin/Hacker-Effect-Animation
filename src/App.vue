29059b312710cd9d710976aa352c9399c696c2ea
sukranyorulmazzzz <sukran_yrlmz@hotmail.com>
2024-05-29 14:05:53 +0300

    tamamlandı

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HackerEffect />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HackerEffect from "./components/HackerEffect.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HackerEffect,
+  },
+};
 </script>
 
 <style>
---
+++ src/components/HackerEffect.vue
@@ -0,0 +1,38 @@
+<template>
+  <h1 data-value="SKANN">SKANN</h1>
+</template>
+
+<script>
+export default {
+  mounted() {
+    const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let iterations = 0;
+
+    const interval = setInterval(() => {
+      const h1 = document.querySelector("h1");
+      h1.innerText = h1.innerText
+        .split("")
+        .map((letter, index) => {
+          if (index < iterations) {
+            return h1.dataset.value[index];
+          }
+          return letters[Math.floor(Math.random() * 26)];
+        })
+        .join("");
+      iterations += 1 / 5;
+      if (iterations >= h1.dataset.value.length) clearInterval(interval);
+    }, 40);
+  },
+};
+</script>
+
+<style>
+body {
+  background-color: black;
+}
+h1 {
+  color: white;
+  font-size: 5rem;
+  font-family: "Space Mono", monospace;
+}
+</style>
